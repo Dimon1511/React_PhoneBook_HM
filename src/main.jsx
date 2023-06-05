2399fc259e0df6486609a73dbe14b1bee3a93c33
Dimon1511 <dimatis0911@gmail.com>
2023-06-05 22:57:52 +0300

    add locale storage

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
 const rootElem = document.querySelector("#root");
 
-createRoot(rootElem).render(<App />);
+createRoot(rootElem).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
