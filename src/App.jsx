6c83827e9ff6038739af964bddb82b9a80700965
Dimon1511 <dimatis0911@gmail.com>
2023-06-06 19:42:00 +0300

    add localeStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
 import styles from "./App.module.css";
 import ContactForm from "./components/ContactForm/ContactForm";
@@ -15,13 +15,7 @@ function initTask() {
 }
 
 function App() {
-  const [contacts, setContacts] = useState([
-    initTask,
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ]);
+  const [contacts, setContacts] = useState(initTask);
   const [filter, setFilter] = useState("");
 
   useEffect(() => {
